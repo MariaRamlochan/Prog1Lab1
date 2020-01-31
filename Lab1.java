c9000cd300840a4bcd9b0a69cdad31c5539a08be
MariaRamlochan <60351622+MariaRamlochan@users.noreply.github.com>
2020-01-31 14:17:10 -0500

    add file

---
+++ Lab1.java
@@ -0,0 +1,29 @@
+public class Lab1 {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+       
+        int a, b;
+        double c;
+
+        a = 5;
+        b = 2;
+
+        a++; // the same as a = a + 2;
+   
+
+        a += 1; // the same as a = a +1;
+        a = a / 2; // the same as a = a / 2;
+
+        c = ((double) a)/b;
+
+         System.out.println("The result of the division is " + c);
+         System.out.println("\nThe result of the division is " + c++);
+         System.out.println("\nThe final value of c " + c);
+
+
+
+
+    }
+
+}
