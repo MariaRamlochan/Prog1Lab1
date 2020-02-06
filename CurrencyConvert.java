86f7335b25070de409c0792d867a3980c3287831
MariaRamlochan <mariaramlochan@hotmail.com>
2020-02-05 21:33:23 -0500

    tried to make a converter for currency... still in progress

---
+++ CurrencyConvert.java
@@ -0,0 +1,16 @@
+  public class CurrencyConvert {
+
+    public static void main(String[] args) {
+        // Variables
+		double CAD_dollar = 100000 ("Dollar");
+		double USD_dollar = 7528.05 ("Dollar");
+		double EURO = 68440.00 ("Euro");
+		
+
+			
+       
+       System.out.println("The current vallue is " + );
+       System.out.println("The will be " + );
+
+
+        
