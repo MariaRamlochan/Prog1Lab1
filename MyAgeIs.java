937fe5b67102a17b54a652821bbeaa3e958c379c
MariaRamlochan <mariaramlochan@hotmail.com>
2020-02-05 20:41:36 -0500

    redoing the MyAgeIs to make sure I understand

--- MyAgeIs.java
+++ MyAgeIs.java
@@ -1,25 +1,18 @@
-public class Lab1 {
+    public class MyAgeIs {
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-       
-        int a, b;
-        double c;
+        // Variables
 
-        a = 5;
-        b = 2;
+        int year, nextYear;
+        double distance;
+        char letter;
+        boolean state;
 
-        a++; // the same as a = a + 2;
-   
+        year = 2020;
+        nextYear = year + 1;
+       System.out.println("The current year is " + year);
+       System.out.println("The next year will be " + nextYear);
 
-        a += 1; // the same as a = a +1;
-        a = a / 2; // the same as a = a / 2;
-
-        c = ((double) a)/b;
-
-         System.out.println("The result of the division is " + c);
-         System.out.println("\nThe result of the division is " + c++);
-         System.out.println("\nThe final value of c " + c);
-
+	}
 
 }
