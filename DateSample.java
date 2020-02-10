1014af70b296a549e6b320a3fa090f5c76652387
MariaRamlochan <mariaramlochan@hotmailcom>
2020-02-10 09:47:48 -0500

    Practice

--- DateSample.java
+++ DateSample.java
@@ -7,6 +7,7 @@ public static void main(String[] args){
 System.out.println("\n**** Date Sample ****");
 
 Date today = new Date();
+Date afterToday = new Date();
 Date year = new Date(120,1,5);
 
 
@@ -15,6 +16,25 @@ System.out.println("\nDate yesterday: " + today.toString());
 System.out.println("\nToday after yesterday: " + today.toString());
 //System.out.println("\nDate today: " + today.getYear());
 //System.out.println("\nDate today: " + today.getMonth());
+
+
+System.out.println("The year of the date is " + afterToday.getYear());
+System.out.println("The month of the date is " + afterToday.getMonth());
+System.out.println("The date of the date is " + afterToday.getDate());
+
+
+
+Date copyToday = today;
+Date cloneToday = (Date)today.clone();
+
+today.setYear(130);
+System.out.println(copyToday);
+System.out.println(today);
+System.out.println(cloneToday);
+
+
+
+
 
 	}
 }
