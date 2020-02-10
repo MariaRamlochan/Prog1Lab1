613c49069d82d278c4cc428cc18fb5f16799f88e
MariaRamlochan <mariaramlochan@hotmailcom>
2020-02-10 10:55:50 -0500

    constant practice

---
+++ ConstantPractice.java
@@ -0,0 +1,14 @@
+public class ConstantPractice{
+
+public static void main(String[] args){
+
+final double PI = 3.14;
+double radius = 2;
+double area = radius * radius * PI;
+
+System.out.println("The are of the circomference of radis " + radius + " is " + area);
+
+
+
+	}
+}
